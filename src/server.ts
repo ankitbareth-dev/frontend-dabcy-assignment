bd91f3b2503706b44af51c9dd3e5fc5f58dbfa95
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-05-07 12:51:34 +0530

    feat: add database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
